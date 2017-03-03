dad63d9e3bfc774761212af9ac5e97a962c623d5
Tony-PC\Tony <tony_whatyougonnado@hotmail.com>
2017-03-03 13:20:41 -0600

    BT50 Constructor & Begin Play Timing

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -13,12 +13,15 @@ ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+
+	auto TankName = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s TEST: Tank C++ Construct"), *TankName);
 }
 
 // Called when the game starts or when spawned
 void ATank::BeginPlay()
 {
-	Super::BeginPlay();
+	Super::BeginPlay(); //Se necesita para que el BP corra
 	
 }
 
